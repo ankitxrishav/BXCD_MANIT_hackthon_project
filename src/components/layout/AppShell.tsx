bec3290821556421fbdd30e68da3680f8a1f5cd2
Chandan Kumar <ck5292363@gmail.com>
2025-11-01 22:19:20 +0000

    bot button isnt visible !!!

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -88,7 +88,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
       <main className="container pt-8 pb-12 px-4 sm:px-6 lg:px-8">
         {children}
       </main>
-      {pathname !== '/dashboard' && <ChatBubble />}
+      <ChatBubble />
     </div>
   );
 }
