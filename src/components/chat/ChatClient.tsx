4c7c1c0df89ed5fd5584dea90551f16b7b929d57
Chandan Kumar <ck5292363@gmail.com>
2025-11-01 19:41:12 +0000

    Try fixing this error: `Console Error: Error: latestSentiment is not def

--- src/components/chat/ChatClient.tsx
+++ src/components/chat/ChatClient.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState } from 'react';
@@ -21,6 +22,7 @@ export default function ChatClient() {
     updateMessage, 
     setMoodSummary, 
     setSuggestions, 
+    latestSentiment, // Added here
     setLatestSentiment, 
     addMoodEntry
   } = useChat();
